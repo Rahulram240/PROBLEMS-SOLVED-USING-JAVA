1cc3172a0d433fd9b6d2005c1ecbc567526f83b6
Rahulram D <105987225+Rahulram240@users.noreply.github.com>
2024-09-05 18:59:23 +0530

    Add files via upload

---
+++ Number_in_array.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Number_in_array {
+    public static int cali(int a[],int q,int d,int r){
+        int res = q*d+r;
+        for(int i=0;i<a.length;i++){
+            if(a[i] == res){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=(int)(Math.random()*100)+1;
+            //a[i]=s.nextInt();
+        }
+        System.err.print("The Elements in the array were ");
+        for(int i:a){
+            System.err.print(i+" ");
+        }
+        System.err.println();
+        System.err.print("Enter the Divisor , Quotient and Remainder =");
+        int q=s.nextInt();
+        int d=s.nextInt();
+        int r=s.nextInt();
+        int resi = cali(a,q,d,r); 
+        System.err.println("The element was at "+resi);
+    }
+}
