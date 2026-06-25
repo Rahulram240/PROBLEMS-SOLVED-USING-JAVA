6039173d5498ea58d224ff87016c650d97da90b6
Rahulram D <105987225+Rahulram240@users.noreply.github.com>
2026-06-25 13:23:59 +0530

    Valid Parentheses--leetcode-20

--- Valid_parentheses.java
+++ Valid_parentheses.java
@@ -1,26 +1,60 @@
-import java.util.Scanner;
+package Longest_Substring;
+
+import java.util.Random;
 import java.util.Stack;
-class Valid_parentheses{
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        String str = s.next();
-        Stack<Character> st = new Stack<>();
-        for(char c:str.toCharArray()){
-            if(c=='('){
-                st.push(c);
-            }
-            else{
-                if(st.isEmpty() || st.pop()!='('){
-                    System.err.println("Its Invalid");
-                    return;
-                }
-            }
-        }
-        if(st.isEmpty()){
-            System.err.println("Its Valid");
-        }
-        else{
-            System.err.println("Its Invalid");
-        }
-}
+
+public class Valid_parentheses {
+	public static void main(String[] a) {
+		String str = generate();
+
+		System.out.println("The String is => " + str);
+
+		System.out.println("This string is " + validParen(str));
+	}
+
+	private static Boolean validParen(String str) {
+		Stack<Character> stack = new Stack<>();
+
+		for (char ch : str.toCharArray()) {
+			if (ch == '(' || ch == '[' || ch == '{') {
+				stack.push(ch);
+			} else {
+				if (stack.isEmpty()) {
+					return false;
+				}
+				char top = stack.pop();
+				if (ch == ')' && top != '(') {
+					return false;
+				}
+				if (ch == ']' && top != '[') {
+					return false;
+				}
+				if (ch == '}' && top != '{') {
+					return false;
+				}
+			}
+		}
+		return stack.isEmpty();
+	}
+
+	private static String generate() {
+		Random r = new Random();
+
+		String open = "([{";
+		String close = ")]}";
+
+		int pairs = r.nextInt(5) + 1;
+
+		StringBuilder left = new StringBuilder();
+		StringBuilder right = new StringBuilder();
+
+		for (int i = 0; i < pairs; i++) {
+			int index = r.nextInt(3);
+
+			left.append(open.charAt(index));
+			right.insert(0, close.charAt(index));
+		}
+
+		return left.toString() + right.toString();
+	}
 }
